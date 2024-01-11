e76f3f6a994f91b4c8fad9c68ac37b64b5d020e3
ohyo555 <555gywn@gmail.com>
2024-01-11 14:50:24 +0900

    15) 로그인 기능 구현, 비밀번호 시도 횟수 제한

---
+++ src/com/KoreaIT/java/JDBCAM/dto/Article.java
@@ -0,0 +1,82 @@
+package com.KoreaIT.java.JDBCAM.dto;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class Article {
+
+	private int id;
+	private LocalDateTime regDate;
+	private LocalDateTime updateDate;
+	private String title;
+	private String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+	public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+	}
+
+	public Article(Map<String, Object> articleMap) {
+		this.id = (int) articleMap.get("id");
+		this.regDate = (LocalDateTime) articleMap.get("regDate");
+		this.updateDate = (LocalDateTime) articleMap.get("updateDate");
+		this.title = (String) articleMap.get("title");
+		this.body = (String) articleMap.get("body");
+	}
+
+	public LocalDateTime getRegDate() {
+		return regDate;
+	}
+
+	public LocalDateTime getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setRegDate(LocalDateTime regDate) {
+		this.regDate = regDate;
+	}
+
+	public void setUpdateDate(LocalDateTime updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
